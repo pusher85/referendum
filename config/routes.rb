a369d56ca10b33c84945293a904d2a466c9e03a9
pusher85 <info@francescobucci.it>
2016-04-10 21:27:40 +0200

    gestire autenticazione - devise installato su model Utenti

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
 
+  devise_for :utentis
   root to: "seggis#index"
 
   #resources :public
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404153520) do
+ActiveRecord::Schema.define(version: 20160410184411) do
 
   create_table "finales", force: :cascade do |t|
     t.integer  "seggi_id",   limit: 4
@@ -69,5 +69,23 @@ ActiveRecord::Schema.define(version: 20160404153520) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
   end
+
+  create_table "utentis", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "utentis", ["email"], name: "index_utentis_on_email", unique: true, using: :btree
+  add_index "utentis", ["reset_password_token"], name: "index_utentis_on_reset_password_token", unique: true, using: :btree
 
 end
